783ece07f4919ed7c5162835c993bf334373a2e2
Kira <winmarattanapairoj@gmail.com>
2026-07-08 13:43:00 +0700

    update

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation"
+import { getSession } from "@/lib/session";
+
+export default async function Dashboard() {
+
+  const user = await getSession ();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+
+  return (
+    <div>
+
+    <h2> Dashboard</h2>
+    <p>Wellcome: {user.name as string} </p>
+    <p>Role: {user.name as string} </p>
+    <p>School: {user.school as string} </p>
+    </div>
+
+  );
+
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +24,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    
+    <html lang="en">
+      
+      <body className="layout">
+       
+        <Navbar />
+        
+        <main className="main-content">
+          
+          {children}
+        
+        </main>
+        
+        <Footer />
+        
+        </body>
+
     </html>
+  
   );
+
 }
